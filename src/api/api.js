9f84e2f934f61c752e4c9f11b21bca769de8ba57
jack20492 <nphuc210@gmail.com>
2024-12-14 18:56:27 +0700

    up lan 2

--- src/api/api.js
+++ src/api/api.js
@@ -1,88 +1,42 @@
-import axios from "axios";
-import { BASE_URL, BASE_URL_2, configHeaders, https, MA_NHOM } from "./config";
+import { https, MA_NHOM } from "./config";
 
 // Movie-related services
 export const getListMovie = () =>
-  axios({
-    url: `${BASE_URL}/QuanLyPhim/LayDanhSachPhim?maNhom=${MA_NHOM}`,
-    method: "GET",
-    headers: configHeaders(),
-  });
+  https.get(`/QuanLyPhim/LayDanhSachPhim?maNhom=${MA_NHOM}`);
 
 export const getDetailMovie = (id) =>
-  axios({
-    url: `${BASE_URL}/QuanLyPhim/LayThongTinPhim?MaPhim=${id}`,
-    method: "GET",
-    headers: configHeaders(),
-  });
+  https.get(`/QuanLyPhim/LayThongTinPhim?MaPhim=${id}`);
 
 export const getDetailMovieShow = (id) =>
-  axios({
-    url: `${BASE_URL}/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${id}`,
-    method: "GET",
-    headers: configHeaders(),
-  });
+  https.get(`/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${id}`);
 
 export const getMovieByTheater = () =>
-  axios({
-    url: `${BASE_URL}/QuanLyRap/LayThongTinLichChieuHeThongRap?maNhom=${MA_NHOM}`,
-    method: "GET",
-    headers: configHeaders(),
-  });
+  https.get(`/QuanLyRap/LayThongTinLichChieuHeThongRap?maNhom=${MA_NHOM}`);
 
-export const getDataSlider = () =>
-  axios({
-    url: `${BASE_URL_2}/QuanLyPhim/LayDanhSachBanner`,
-    method: "GET",
-    headers: configHeaders(),
-  });
+export const getDataSlider = () => https.get(`/QuanLyPhim/LayDanhSachBanner`);
 
 // Ticket-related services
 export const postTickets = (id, seats, info) =>
-  axios({
-    url: `${BASE_URL}/QuanLyDatVe/DatVe`,
-    method: "POST",
-    headers: {
-      ...configHeaders(),
-      Authorization: `Bearer ${info.accessToken}`,
-    },
-    data: {
-      maLichChieu: id,
-      danhSachVe: seats,
-      taiKhoanNguoiDung: info.taiKhoan,
-    },
-  });
-
-export const getSeatListByFilm = (id) =>
-  axios({
-    url: `${BASE_URL}/QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=${id}`,
-    method: "GET",
-    headers: configHeaders(),
+  https.post(`/QuanLyDatVe/DatVe`, {
+    maLichChieu: id,
+    danhSachVe: seats,
+    taiKhoanNguoiDung: info.taiKhoan,
   });
 
 // User-related services
+export const getSeatListByFilm = (id) =>
+  https.get(`/QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=${id}`);
+
 export const getUserInfo = (userName) =>
-  axios({
-    url: `${BASE_URL}/QuanLyNguoiDung/TimKiemNguoiDung?MaNhom=${MA_NHOM}&tuKhoa=${userName}`,
-    method: "GET",
-    headers: configHeaders(),
-  });
+  https.get(
+    `/QuanLyNguoiDung/TimKiemNguoiDung?MaNhom=${MA_NHOM}&tuKhoa=${userName}`
+  );
 
 export const layUserTickets = (userName) =>
-  axios({
-    url: `${BASE_URL}/QuanLyNguoiDung/ThongTinTaiKhoan`,
-    method: "POST",
-    headers: configHeaders(),
-    data: { taiKhoan: userName },
-  });
+  https.post(`/QuanLyNguoiDung/ThongTinTaiKhoan`, { taiKhoan: userName });
 
-export const putUserInfo = (values, accessToken) =>
-  axios({
-    url: `${BASE_URL}/QuanLyNguoiDung/CapNhatThongTinNguoiDung`,
-    method: "PUT",
-    headers: { ...configHeaders(), Authorization: `Bearer ${accessToken}` },
-    data: values,
-  });
+export const putUserInfo = (values) =>
+  https.put(`/QuanLyNguoiDung/CapNhatThongTinNguoiDung`, values);
 
 // Admin services
 export const userServ = {
